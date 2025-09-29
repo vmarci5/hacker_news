6e373a981c4872dc88b6025d43d4a1094daea453
Varhelyi Marton <varhelyimarton5@gmail.com>
2025-09-29 20:16:44 +0200

    add suspense to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Card from '@/components/content/card/page';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <>
+    <Suspense>
       <Card />
+    </Suspense>
     </>
   );
 }
